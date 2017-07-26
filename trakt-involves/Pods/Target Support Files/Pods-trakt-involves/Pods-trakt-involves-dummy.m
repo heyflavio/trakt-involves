8fd118c999965c168b4af8c7cf473b0cd78a0930
Flavio Kruger Bittencourt <flavio.bittenca@gmail.com>
2017-07-26 16:23:28 -0300

    Integrates cocoapods to project.

---
+++ trakt-involves/Pods/Target Support Files/Pods-trakt-involves/Pods-trakt-involves-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_trakt_involves : NSObject
+@end
+@implementation PodsDummy_Pods_trakt_involves
+@end
---
+++ trakt-involves/Pods/Target Support Files/Pods-trakt-involvesTests/Pods-trakt-involvesTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_trakt_involvesTests : NSObject
+@end
+@implementation PodsDummy_Pods_trakt_involvesTests
+@end
