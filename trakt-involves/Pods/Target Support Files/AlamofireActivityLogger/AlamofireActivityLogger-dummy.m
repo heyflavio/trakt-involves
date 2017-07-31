71fd0818f3f8f088bf1042b09a12ec4d0570ed3e
Flavio Kruger Bittencourt <flavio.bittenca@gmail.com>
2017-07-31 16:52:52 -0300

    Remove pods from repo.

--- trakt-involves/Pods/Target Support Files/AlamofireActivityLogger/AlamofireActivityLogger-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_AlamofireActivityLogger : NSObject
-@end
-@implementation PodsDummy_AlamofireActivityLogger
-@end
--- trakt-involves/Pods/Target Support Files/Pods-trakt-involvesTests/Pods-trakt-involvesTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_trakt_involvesTests : NSObject
-@end
-@implementation PodsDummy_Pods_trakt_involvesTests
-@end
